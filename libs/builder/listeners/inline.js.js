282258869b40c38b5e0ae27c5ce11326bbdd97d8
Ric Allinson <git@ricallinson.com>
2023-03-10 18:51:05 -0800

    Changed asset data model

--- libs/builder/listeners/inline.js.js
+++ libs/builder/listeners/inline.js.js
@@ -1,13 +1,13 @@
 const fs = require('fs');
+const path = require('path');
 
 const Conf = require('global-conf');
 
 Conf.listen.set('builder.loadAllPageConfigurations.end', function(loc, val) {
     Conf.getDescendantLocations('.', 'js.files').forEach(function(location) {
         Conf.get(location).forEach(function(filePath) {
-            const content = Conf.get(location, '<<', 'inline') || '';
             const data = fs.readFileSync(filePath, 'utf8');
-            Conf.set(location, '<<', 'inline', content + data);
+            Conf.set(location, '<<', 'inline', Conf.normalizeKey(path.basename(filePath)), data);
         });
     });
 });
